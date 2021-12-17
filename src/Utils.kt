3a208169c02897dddb4c2ec4bd9007b98fd2ea8a
Kristian Thun <kristian.thun@sintef.no>
2021-12-17 16:05:14 +0100

    Day 9 part 1 and started day 11

--- src/Utils.kt
+++ src/Utils.kt
@@ -15,7 +15,20 @@ fun readInputAsInts(name: String) = File("src", "$name.txt").readLines().map { i
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
 data class Coord(val x: Int, val y: Int) {
+    fun orthogonalNeighbours(): List<Coord> {
+        return listOf(
+            Coord(x, y - 1),
+            Coord(x, y + 1),
+            Coord(x - 1, y),
+            Coord(x + 1, y),
+        )
+    }
     fun neighbours(): List<Coord> {
-        TODO()
+        return orthogonalNeighbours() + listOf(
+            Coord(x - 1, y - 1),
+            Coord(x - 1, y + 1),
+            Coord(x + 1, y - 1),
+            Coord(x + 1, y + 1),
+        )
     }
 }
